cc3d869d94dec4552e23cbf05547d1bb2b2eacaf
Jorge E. <jorge.espadas@hotmail.com>
2026-01-04 12:06:51 -0600

    Update: Fixed some dumb stuff.

--- src/main/java/dev/ezpadaz/vanillaPlus/Features/DoubleXP/Listener/ASDoubleXPListener.java
+++ src/main/java/dev/ezpadaz/vanillaPlus/Features/DoubleXP/Listener/ASDoubleXPListener.java
@@ -3,9 +3,13 @@ package dev.ezpadaz.vanillaPlus.Features.DoubleXP.Listener;
 import dev.aurelium.auraskills.api.event.skill.DamageXpGainEvent;
 import dev.aurelium.auraskills.api.event.skill.EntityXpGainEvent;
 import dev.aurelium.auraskills.api.event.skill.XpGainEvent;
+import dev.ezpadaz.vanillaPlus.Features.DoubleXP.DoubleXP;
 import dev.ezpadaz.vanillaPlus.Utils.GeneralHelper;
+import dev.ezpadaz.vanillaPlus.Utils.MessageHelper;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+
+import java.util.Locale;
 
 public class ASDoubleXPListener implements Listener {
 
@@ -27,8 +31,7 @@ public class ASDoubleXPListener implements Listener {
 
     public double gainExpHandler(double amount){
         int multiplier = GeneralHelper.getConfigInt("features.double-xp.multiplier");
-        boolean isEnabled = GeneralHelper.getConfigBool("features.double-xp.enabled");
-
+        boolean isEnabled = DoubleXP.isEventEnabled();
         if(isEnabled) return (amount * multiplier);
         return amount;
     }
